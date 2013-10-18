83496035fdfd35b9da26921ba8ffc3aea6fe9663
Sarnab <sarnab.poddar@gmail.com>
2013-10-18 02:13:51 -0700

    added to last CL

--- AfterUI/src/com/beacon/afterui/sliding/customViews/CustomGridView.java
+++ AfterUI/src/com/beacon/afterui/sliding/customViews/CustomGridView.java
@@ -1,8 +1,10 @@
 package com.beacon.afterui.sliding.customViews;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridView;
 
 /**
@@ -10,13 +12,19 @@ import android.widget.GridView;
  */
 public class CustomGridView extends GridView {
 
-	private float mLastY = -1; 
+	private float mLastY = -1, mx, my , curX, curY; 
+	boolean click = false;
 
 	private IDataListener mDataListener;
 
 	private boolean mEnablePullLoad = true;
 
 	private int mCapacity = 50;
+	
+	/**
+	 * Rectangle used for hit testing children
+	 */
+	private Rect mTouchFrame;
 
 	public CustomGridView(Context context) {
 		super(context);
@@ -50,6 +58,8 @@ public class CustomGridView extends GridView {
 	public boolean onTouchEvent(MotionEvent ev) {
 		if(this.isEnabled() 
 				&& getAdapter() != null) {
+			
+			
 			if (mLastY == -1) {
 				mLastY = ev.getRawY();
 			}
@@ -57,10 +67,22 @@ public class CustomGridView extends GridView {
 			switch (ev.getAction()) {
 			case MotionEvent.ACTION_DOWN:
 				mLastY = ev.getRawY();
+				click = true;
+                mx = ev.getX();
+                my = ev.getY();                     
 				break;
 			case MotionEvent.ACTION_MOVE:
 				final float deltaY = ev.getRawY() - mLastY;
 				mLastY = ev.getRawY();
+				curX = ev.getX();
+				curY = ev.getY();
+				if(click && (mx-curX <5) && (my-curY <5))
+				{
+					click = true;
+				}
+				else{
+					click = false;
+				}
 				if (getLastVisiblePosition() == getAdapter().getCount() - 1 
 						&& deltaY < 0) {
 					if(mEnablePullLoad 
@@ -69,12 +91,60 @@ public class CustomGridView extends GridView {
 					}
 				}
 				break;
+			case MotionEvent.ACTION_UP:
+				if(click)
+				{
+//					int position = pointToPosition(mx, my);
+//					if (position>=0 && position<=getCount()) {
+//						final View view = getChildAt(position);
+//						// If there is no view, something bad happened (the view
+//						// scrolled off the
+//						// screen, etc.) and we should cancel the click
+//						if (view != null) {
+//							performItemClick(view, position,
+//									getItemIdAtPosition(position));
+//						}
+//                    }
+				}
+				break;
 			default:
 				mLastY = -1;
 				break;
 			}
 		}
 		return super.onTouchEvent(ev);
+	}
+	
+	public int pointToPosition(float x, float y) {
+		Rect frame = mTouchFrame;
+		if (frame == null) {
+			mTouchFrame = new Rect();
+			frame = mTouchFrame;
+		}
+
+		final int count = getChildCount();
+		for (int i = count - 1; i >= 0; i--) {
+			final View child = getChildAt(i);
+			if (child.getVisibility() == View.VISIBLE) {
+				child.getHitRect(frame);
+				if (frame.contains((int)x, (int)y)) {
+					return i;
+				}
+			}
+		}
+		return INVALID_POSITION;
+	}
+	
+	@Override
+	public View getChildAt(int position)
+	{
+		final int count = getChildCount();
+		if(position > count-1)
+		{
+			position = getLastVisiblePosition() - position;
+			
+		}
+		return super.getChildAt(position);
 	}
 
 	public void setCapacity(int capacity) {
