b7db3b45a3f481938f576a9b3e0850ec568a7021
skadu <sushil.system@gmail.com>
2013-12-08 19:16:34 +0530

    Adding initial flow for InAppBilling

---
+++ AfterUI/src/com/beacon/afterui/inappbilling/util/InAppBillingConstants.java
@@ -0,0 +1,8 @@
+package com.beacon.afterui.inappbilling.util;
+
+public interface InAppBillingConstants {
+
+    public static final String APP_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjLkU68k5Ka05lj4oY6G8cUI9doN7aJGaKWqCX2/QLdTUKeCoc5nrcmQKvY5A8hEfQO2GwzyEJDDb3q/ZfVmYAf8lpOqBZ1FoUrx+uu82EVrfPfpqOhsue8L9q2NeHBkvv7kUps8Y4f0l5ERy1JelvWOYqug8cgCMr6B8gZwvkyd9vLOJSVVIQ4Huk/Y8K+DAQjPpIZyNwXqchSiCYe+XKYwlBHk9S1vBntMGXdkilgXRVQbAcu7tsOvfPw9jOPSXSKZ6wm/ndW2MC9URLH2MgpufRj0Vrl7qm4ldWXlCzTsvALMvkFTdP69VI2iZ4NjFecx43mkG2WRcBfrXSN1+8wIDAQAB";
+    
+    public static final String FIVE_K = "5000";
+}
--- AfterUI/src/com/beacon/afterui/sliding/fragment/GiftDetailsFragment.java
+++ AfterUI/src/com/beacon/afterui/sliding/fragment/GiftDetailsFragment.java
@@ -1,21 +1,39 @@
 package com.beacon.afterui.sliding.fragment;
 
-import com.beacon.afterui.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.beacon.afterui.R;
+import com.beacon.afterui.constants.CommonConstants;
 
 public class GiftDetailsFragment extends Fragment implements FragmentLifecycle,
-        ISearchFunction {
+        ISearchFunction, OnClickListener {
+
+    /** TAG */
+    private static final String TAG = GiftDetailsFragment.class.getSimpleName();
+
+    private Button mDoneButton;
+    private Button mCancelButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.gift_details_screen, null, false);
+        View view = inflater.inflate(R.layout.gift_details_screen, null, false);
+
+        mDoneButton = (Button) view.findViewById(R.id.gift_details_done_button);
+        mCancelButton = (Button) view
+                .findViewById(R.id.gift_details_cancel_button);
+
+        mDoneButton.setOnClickListener(this);
+        mCancelButton.setOnClickListener(this);
+
+        return view;
     }
 
     @Override
@@ -25,19 +43,35 @@ public class GiftDetailsFragment extends Fragment implements FragmentLifecycle,
 
     @Override
     public void onFragmentPause() {
-        
+
     }
 
     @Override
     public void onFragmentResume() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public boolean onBack() {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.gift_details_done_button:
+            Fragment detail = new GiftsBuyFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt(CommonConstants.BundleKey.GIFT_ID, 1);
+            FragmentHelper.gotoFragment(getActivity(),
+                    GiftDetailsFragment.this, detail, bundle);
+            break;
+
+        case R.id.gift_details_cancel_button:
+            break;
+        }
     }
 
 }
