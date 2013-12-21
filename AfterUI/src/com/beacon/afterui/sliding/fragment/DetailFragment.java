065fe1f440761b93583ebcf9efc881187e32b815
skadu <sushil.system@gmail.com>
2013-12-21 23:40:03 +0530

    Adding DetailFragment changes.

--- AfterUI/src/com/beacon/afterui/sliding/fragment/DetailFragment.java
+++ AfterUI/src/com/beacon/afterui/sliding/fragment/DetailFragment.java
@@ -15,7 +15,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.beacon.afterui.R;
@@ -41,21 +43,42 @@ public class DetailFragment extends Fragment implements FragmentLifecycle,
 	private boolean isBacking = false;
 
 	private Context mContext;
+	private int mNumNotification = 25;
+	private int mNumHot = 25;
+	private int mNumComment = 3;
 
-	Typeface typeFaceRegular;
+	private Typeface mMyriadProRegularFont;
 	Typeface typeFaceBold;
 	Typeface typefaceBlack;
 	Typeface typefaceItalic;
+
+	private TextView mVoteBtn;
+	private TextView mConnectBtn;
+	private TextView mRefferBtn;
+	private TextView mChatBtn;
+
 	private TextView mPhotosBtn;
 	private TextView mGiftDetailBtn;
 	private TextView mVideoDetailBtn;
 	private TextView mCommentBtn;
+
+	private TextView mNotificationTxt;
+	private TextView mEditBtn;
+	private TextView mNumHotTxt;
+	private TextView mHomeLocationTxt;
+	private TextView mFromLocation;
+	private TextView mUserName;
+
+	private ListView mCommentList;
+
+	private LayoutInflater mInflater;
+
 	private Typeface mITCAvantGardeStdBk;
 
 	public DetailFragment(Context mContext) {
 		this.mContext = mContext;
-		typeFaceRegular = Typeface.createFromAsset(mContext.getAssets(),
-				"fonts/MyriadPro-Regular.otf");
+		mMyriadProRegularFont = FontUtils.loadTypeFace(this.mContext,
+				"MyriadPro-Regular.otf");
 		typeFaceBold = Typeface.createFromAsset(mContext.getAssets(),
 				"fonts/MyriadPro-Semibold.otf");
 		typefaceBlack = Typeface.createFromAsset(mContext.getAssets(),
@@ -78,15 +101,117 @@ public class DetailFragment extends Fragment implements FragmentLifecycle,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.detail_box, null);
 		mDetailView = (ViewGroup) view.findViewById(R.id.detail_content);
+
+		mCommentList = (ListView) mDetailView.findViewById(R.id.comment_list);
+		mCommentList.setAdapter(new CommentAdapter());
+		Helper.getListViewSize(mCommentList);
+
 		mPhotosBtn = (TextView) view.findViewById(R.id.photos_btn);
+		mPhotosBtn.setTypeface(mITCAvantGardeStdBk);
+
 		mGiftDetailBtn = (TextView) view.findViewById(R.id.gift_btn);
+		mGiftDetailBtn.setTypeface(mITCAvantGardeStdBk);
+
 		mVideoDetailBtn = (TextView) view.findViewById(R.id.video_btn);
+		mVideoDetailBtn.setTypeface(mITCAvantGardeStdBk);
+
 		mCommentBtn = (TextView) view.findViewById(R.id.comment_btn);
+		mCommentBtn.setTypeface(mITCAvantGardeStdBk);
+
+		mVoteBtn = (TextView) view.findViewById(R.id.vote_btn_detail);
+		mVoteBtn.setTypeface(mITCAvantGardeStdBk);
+
+		mConnectBtn = (TextView) view.findViewById(R.id.connect_btn_detail);
+		mVoteBtn.setTypeface(mITCAvantGardeStdBk);
+
+		mRefferBtn = (TextView) view.findViewById(R.id.referr_btn_detail);
+		mConnectBtn.setTypeface(mITCAvantGardeStdBk);
+
+		mChatBtn = (TextView) view.findViewById(R.id.chat_btn_detail);
+		mChatBtn.setTypeface(mITCAvantGardeStdBk);
+
+		mNotificationTxt = (TextView) view.findViewById(R.id.notification_txt);
+		mNotificationTxt.setText("Notifications " + "(" + mNumNotification
+				+ ")");
+		mNotificationTxt.setTypeface(mITCAvantGardeStdBk);
+
+		// edit btn on img
+		mEditBtn = (TextView) view.findViewById(R.id.edit_btn);
+		mEditBtn.setTypeface(mITCAvantGardeStdBk);
+		mEditBtn.setOnClickListener(this);
+
+		// num hot
+		mNumHotTxt = (TextView) view.findViewById(R.id.num_hot);
+		mNumHotTxt.setTypeface(mITCAvantGardeStdBk);
+		mNumHotTxt.setText("Hot " + "(" + mNumHot + ")");
+
+		TextView last_update_txt = (TextView) view
+				.findViewById(R.id.last_update_txt);
+		last_update_txt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView lastUpdateStatus = (TextView) view
+				.findViewById(R.id.last_update_status);
+		lastUpdateStatus.setTypeface(mITCAvantGardeStdBk);
+
+		TextView home_txt = (TextView) view.findViewById(R.id.home_txt);
+		home_txt.setTypeface(mITCAvantGardeStdBk);
+
+		mHomeLocationTxt = (TextView) view.findViewById(R.id.home_location);
+		mHomeLocationTxt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView from_txt = (TextView) view.findViewById(R.id.from_txt);
+		from_txt.setTypeface(mITCAvantGardeStdBk);
+
+		mFromLocation = (TextView) view.findViewById(R.id.from_location);
+		mFromLocation.setTypeface(mITCAvantGardeStdBk);
+
+		TextView edit_txt = (TextView) view.findViewById(R.id.edit_txt);
+		edit_txt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView sponser_txt = (TextView) view.findViewById(R.id.sponser_txt);
+		sponser_txt.setTypeface(mITCAvantGardeStdBk);
+
+		mUserName = (TextView) view.findViewById(R.id.user_name);
+		mUserName.setTypeface(mITCAvantGardeStdBk);
+
+		TextView and_txt = (TextView) view.findViewById(R.id.and_txt);
+		and_txt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView other_txt = (TextView) view.findViewById(R.id.other_txt);
+		other_txt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView think_txt = (TextView) view.findViewById(R.id.think_txt);
+		think_txt.setTypeface(mITCAvantGardeStdBk);
+
+		TextView comments_txt = (TextView) view.findViewById(R.id.comments_txt);
+		comments_txt.setTypeface(mITCAvantGardeStdBk);
+		comments_txt.setText("Comments " + "(" + mNumComment + ")");
+
+		TextView my_story_label = (TextView) view
+				.findViewById(R.id.my_story_label);
+		my_story_label.setTypeface(mITCAvantGardeStdBk);
+
+		TextView my_story_text = (TextView) view
+				.findViewById(R.id.my_story_text);
+		my_story_text.setTypeface(mMyriadProRegularFont);
+
+		TextView my_match_label = (TextView) view
+				.findViewById(R.id.my_match_label);
+		my_match_label.setTypeface(mITCAvantGardeStdBk);
+
+		TextView my_match_text = (TextView) view
+				.findViewById(R.id.my_match_text);
+		my_match_text.setTypeface(mMyriadProRegularFont);
 
 		mPhotosBtn.setOnClickListener(this);
 		mGiftDetailBtn.setOnClickListener(this);
 		mVideoDetailBtn.setOnClickListener(this);
 		mCommentBtn.setOnClickListener(this);
+
+		mVoteBtn.setOnClickListener(this);
+		mConnectBtn.setOnClickListener(this);
+		mRefferBtn.setOnClickListener(this);
+		mChatBtn.setOnClickListener(this);
 
 		if (getArguments() != null) {
 			mCacheKey = getArguments().getInt(
@@ -119,6 +244,22 @@ public class DetailFragment extends Fragment implements FragmentLifecycle,
 
 			break;
 
+		case R.id.vote_btn:
+
+			break;
+		case R.id.connect_btn_detail:
+
+			break;
+		case R.id.referr_btn_detail:
+
+			break;
+		case R.id.chat_btn_detail:
+
+			break;
+		case R.id.edit_txt:
+
+			break;
+
 		}
 
 	}
@@ -139,11 +280,15 @@ public class DetailFragment extends Fragment implements FragmentLifecycle,
 			ImageUtils.getInstance(getActivity()).resizeImage(
 					data.getDataSrc(), iv);
 		}
+		// Detail view
+
 		TextView nameView = (TextView) mDetailView
 				.findViewById(R.id.detail_name);
 		nameView.setTypeface(mITCAvantGardeStdBk);
+
 		TextView ageView = (TextView) mDetailView.findViewById(R.id.detail_age);
 		ageView.setTypeface(mITCAvantGardeStdBk);
+
 		// TextView albumCount = (TextView) mDetailView
 		// .findViewById(R.id.detail_album_count);
 		// albumCount.setTypeface(mITCAvantGardeStdBk);
@@ -286,6 +431,75 @@ public class DetailFragment extends Fragment implements FragmentLifecycle,
 		} else {
 			return height < width ? height : width;
 		}
+	}
+
+	private class CommentAdapter extends BaseAdapter {
+
+		public CommentAdapter() {
+
+			mInflater = LayoutInflater.from(mContext);
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View view, ViewGroup parent) {
+
+			ViewHolder holder;
+
+			if (view == null) {
+				holder = new ViewHolder();
+				view = mInflater.inflate(R.layout.comments_item_detail, null);
+
+				holder.commentTxt = (TextView) view
+						.findViewById(R.id.comment_txt);
+				holder.commentTxt.setTypeface(mMyriadProRegularFont);
+
+				holder.commentUserImg = (ImageView) view
+						.findViewById(R.id.comment_user_img);
+
+				holder.commentUserName = (TextView) view
+						.findViewById(R.id.comment_user_name);
+				holder.commentUserName.setTypeface(mITCAvantGardeStdBk);
+
+				holder.timeTxt = (TextView) view
+						.findViewById(R.id.comment_time_txt);
+				holder.timeTxt.setTypeface(mMyriadProRegularFont);
+
+				view.setTag(holder);
+
+			} else {
+
+				holder = (ViewHolder) view.getTag();
+			}
+
+			return view;
+		}
+
+		private class ViewHolder {
+
+			private ImageView commentUserImg;
+			private TextView commentUserName;
+			private TextView commentTxt;
+			private TextView timeTxt;
+
+		}
+
 	}
 
 }
